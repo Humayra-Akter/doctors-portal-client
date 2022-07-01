eccfb4553eb46b2727d7d50ded25a240828aabf9
humayra akter <humayraakter25@gmail.com>
2022-07-01 21:04:55 +0600

    login field & react hook form

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -12,24 +12,24 @@ const BookingModal = ({ treatment, date, setTreatment }) => {
 
         return (
                 <div>
-                        <input type="checkbox" id="booking-modal" class="modal-toggle" />
-                        <div class="modal modal-bottom sm:modal-middle">
-                                <div class="modal-box">
-                                        <label for="booking-modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                                        <h3 class="font-bold text-lg text-accent">{name}</h3>
+                        <input type="checkbox" id="booking-modal" className="modal-toggle" />
+                        <div className="modal modal-bottom sm:modal-middle">
+                                <div className="modal-box">
+                                        <label for="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                                        <h3 className="font-bold text-lg text-accent">{name}</h3>
                                         <form
                                                 onSubmit={handleBooking}
                                                 className='justify-items-center grid grid-cols-1 gap-3 mt-4'>
-                                                <input type="text" disabled value={format(date, 'pp')} class="input input-bordered w-full max-w-xs" />
-                                                <select class="select select-bordered w-full max-w-xs">
+                                                <input type="text" disabled value={format(date, 'pp')} className="input input-bordered w-full max-w-xs" />
+                                                <select className="select select-bordered w-full max-w-xs">
                                                         {
                                                                 slots.map(slot => <option value={slot}>{slot}</option>)
                                                         }
                                                 </select>
-                                                <input type="text" name="name" placeholder="Your Name" class="input input-bordered w-full max-w-xs" />
-                                                <input type="email" name="email" placeholder="Email Address" class="input input-bordered w-full max-w-xs" />
-                                                <input type="text" name="phone" placeholder="Phone Number" class="input input-bordered w-full max-w-xs" />
-                                                <input type="submit" value="submit" class="btn btn-accent w-full max-w-xs" />
+                                                <input type="text" name="name" placeholder="Your Name" className="input input-bordered w-full max-w-xs" />
+                                                <input type="email" name="email" placeholder="Email Address" className="input input-bordered w-full max-w-xs" />
+                                                <input type="text" name="phone" placeholder="Phone Number" className="input input-bordered w-full max-w-xs" />
+                                                <input type="submit" value="submit" className="btn btn-accent w-full max-w-xs" />
                                         </form>
                                 </div>
                         </div>
--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Service = ({ service, setTreatment }) => {
         const { name, slots } = service;
         return (
-                <div class="card lg:max-w-lg bg-base-100 shadow-xl">
-                        <div class="card-body text-center">
-                                <h2 class="card-title" className='text-xl text-center text-secondary font-bold'>{name}</h2>
+                <div className="card lg:max-w-lg bg-base-100 shadow-xl">
+                        <div className="card-body text-center">
+                                <h2 className="card-title" className='text-xl text-center text-secondary font-bold'>{name}</h2>
                                 <p>
                                         {
                                                 slots.length > 0 ?
@@ -15,12 +15,12 @@ const Service = ({ service, setTreatment }) => {
                                         }
                                 </p>
                                 <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
-                                <div class="card-actions justify-center">
+                                <div className="card-actions justify-center">
                                         <label
                                                 for="booking-modal"
                                                 onClick={() => setTreatment(service)}
                                                 disabled={slots.length === 0}
-                                                class="btn btn-sm btn-secondary text-white uppercase bg-gradient-to-r from-secondary to-primary">Book Appointment
+                                                className="btn btn-sm btn-secondary text-white uppercase bg-gradient-to-r from-secondary to-primary">Book Appointment
                                         </label>
                                 </div>
                         </div>
